ee04bbe25d9c5f924ad207adde9bdddb303407d1
Brirosman <brirosman10@gmail.com>
2024-06-25 09:04:03 -0300

    .
    .

---
+++ Assets/Precios.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Precios : MonoBehaviour
+{
+    public GameObject[] precios;
+    
+    void Start()
+    {
+        
+    }
+
+    
+    void Update()
+    {
+        
+    }
+}
